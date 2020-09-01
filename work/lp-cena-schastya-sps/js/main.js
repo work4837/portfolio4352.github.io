bfa25c3eaba113e8ee75838d915d2581318d7a86
portfolio4352 <63583778+portfolio4352@users.noreply.github.com>
2020-09-01 19:53:39 +0300

    1

--- work/lp-cena-schastya-sps/js/main.js
+++ work/lp-cena-schastya-sps/js/main.js
@@ -1,3 +1,40 @@
 $(function() {
+    $('.hapiness__bg').lettering();
 
+    $('.situations__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        swipe: false,
+        prevArrow: '<div class="tap-area left-area"></div>',
+        nextArrow: '<div class="tap-area right-area"></div>',
+        responsive: [
+            {
+                breakpoint: 1150,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                    swipe: true,
+                    centerMode: true,
+                    centerPadding: '20px',
+                    dots: true
+                }  
+            },
+            {
+                breakpoint: 380,
+                settings: {
+                    slidesToShow: 1,
+                    swipe: true,
+                    centerMode: true,
+                    centerPadding: '25px',
+                    dots: true
+                }
+            }
+        ]
+    });
 });
